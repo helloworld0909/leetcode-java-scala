751da747e967a43a52a429e67c59e43d823964b4
luozheng_sx <luozheng_sx@qiyi.com>
2018-03-06 14:04:19 +0800

    Replace Array with Seq for better .tail performance

--- src/main/scala/com/helloworld09/leetcode/scala/LC738.scala
+++ src/main/scala/com/helloworld09/leetcode/scala/LC738.scala
@@ -3,14 +3,14 @@ package com.helloworld09.leetcode.scala
 object LC738 {
   //738. Monotone Increasing Digits
 
-  def monotoneIncreasingConflictIdx(N: Array[Int]): Int = {
-    if (N.length <= 1)
+  def monotoneIncreasingConflictIdx(N: Seq[Int]): Int = {
+    if (N.lengthCompare(1) <= 0)
       -1
-    else if (N.head > N.tail(0))
+    else if (N.head > N.tail.head)
       N.length - 1
     else {
       val tailConfilctIdx = monotoneIncreasingConflictIdx(N.tail)
-      if (N.head == N.tail(0) & tailConfilctIdx == N.length - 2)
+      if (N.head == N.tail.head & tailConfilctIdx == N.length - 2)
         N.length - 1
       else
         tailConfilctIdx
@@ -19,16 +19,16 @@ object LC738 {
 
   def monotoneIncreasingDigits(N: Int): Int = {
 
-    implicit def num2digits(N: Int): Array[Int] = {
+    implicit def num2digits(N: Int): Seq[Int] = {
       N.toString.toCharArray.map(_.toInt - '0'.toInt)
     }
 
-    implicit def digits2num(array: Array[Int]): Int = {
+    implicit def digits2num(array: Seq[Int]): Int = {
       array.map(_.toString).mkString.toInt
     }
 
     val conflictIdx = monotoneIncreasingConflictIdx(N)
-    val newDigits: Array[Int] = num2digits(N).reverse.zipWithIndex.map {
+    val newDigits: Seq[Int] = num2digits(N).reverse.zipWithIndex.map {
       case (digit, idx) =>
         if (idx < conflictIdx)
           9
