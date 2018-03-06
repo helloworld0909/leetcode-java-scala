aee6f1b5c763efcb6a1511063e985fc355c807c5
luozheng_sx <luozheng_sx@qiyi.com>
2018-03-06 11:37:58 +0800

    Refactor for multi langs

---
+++ src/main/java/com/helloworld09/leetcode/java/LC540.java
@@ -0,0 +1,42 @@
+package com.helloworld09.leetcode.java;
+
+
+public class LC540 {
+    public int singleNonDuplicate(int[] nums) {
+        int idx = findSingle(nums, 0, nums.length - 1);
+        return nums[idx];
+    }
+
+    private int findSingle(int[] nums, int start, int end) {
+        if (start == end)
+            return start;
+        else if (start > end || start + 1 == end)
+            return -1;
+        else {
+            int mid = (start + end) / 2;
+            if (nums[mid] == nums[mid + 1]) {
+                int leftRet = findSingle(nums, start, mid - 1);
+                if (leftRet != -1)
+                    return leftRet;
+                int rightRet = findSingle(nums, mid + 2, end);
+                if (rightRet != -1)
+                    return rightRet;
+            } else if (nums[mid] == nums[mid - 1]) {
+                int leftRet = findSingle(nums, start, mid - 2);
+                if (leftRet != -1)
+                    return leftRet;
+                int rightRet = findSingle(nums, mid + 1, end);
+                if (rightRet != -1)
+                    return rightRet;
+            } else
+                return mid;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        LC540 solution = new LC540();
+        int[] input = new int[]{1, 1, 2};
+        System.out.println(solution.singleNonDuplicate(input));
+    }
+}
