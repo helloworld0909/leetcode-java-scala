36c81d2f3d092cdc4efa48e5cb852d813287a0b1
luozheng_sx <luozheng_sx@qiyi.com>
2018-04-18 14:52:12 +0800

    Modify LeetCode Interval, add a factory method

--- src/main/java/com/helloworld09/leetcode/java/util/Interval.java
+++ src/main/java/com/helloworld09/leetcode/java/util/Interval.java
@@ -1,5 +1,7 @@
 package com.helloworld09.leetcode.java.util;
 
+import java.util.*;
+
 public class Interval {
     public int start;
     public int end;
@@ -17,5 +19,29 @@ public class Interval {
     @Override
     public String toString() {
         return String.format("[%d, %d]", start, end);
+    }
+
+    public static List<Interval> getIntervalList(int... inputList) {
+        List<Interval> ret = new ArrayList<>();
+        if (inputList.length % 2 != 0) {
+            System.out.println("Num of input params should be even");
+            return ret;
+        } else {
+            for (int i = 0; i < inputList.length; i += 2) {
+                int start = inputList[i];
+                int end = inputList[i + 1];
+                if (start > end) {
+                    int tmp = start;
+                    start = end;
+                    end = tmp;
+                }
+                ret.add(new Interval(start, end));
+            }
+            return ret;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getIntervalList(1, 2, 4, 5));
     }
 }
