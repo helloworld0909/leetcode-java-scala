bc201e12eafade0191c5f760a85d44efd067084b
luozheng_sx <luozheng_sx@qiyi.com>
2018-04-18 17:18:13 +0800

    Add description

--- src/main/java/com/helloworld09/leetcode/java/LC540.java
+++ src/main/java/com/helloworld09/leetcode/java/LC540.java
@@ -2,6 +2,17 @@ package com.helloworld09.leetcode.java;
 
 
 public class LC540 {
+    /**
+     * Given a sorted array consisting of only integers where every element appears twice except for one element which appears once. Find this single element that appears only once.
+     * <p>
+     * Example 1:
+     * Input: [1,1,2,3,3,4,4,8,8]
+     * Output: 2
+     * Example 2:
+     * Input: [3,3,7,7,10,11,11]
+     * Output: 10
+     * Note: Your solution should run in O(log n) time and O(1) space.
+     */
     public int singleNonDuplicate(int[] nums) {
         int idx = findSingle(nums, 0, nums.length - 1);
         return nums[idx];
