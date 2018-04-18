f1f5923e79983daabdae09f34b4e8e93d00c7fb7
luozheng_sx <luozheng_sx@qiyi.com>
2018-04-18 14:39:08 +0800

    Modified the LeetCode ListNode implementation for convenience

--- src/main/java/com/helloworld09/leetcode/java/util/ListNode.java
+++ src/main/java/com/helloworld09/leetcode/java/util/ListNode.java
@@ -7,4 +7,26 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    @Override
+    public String toString() {
+        if (next == null)
+            return String.valueOf(val) + " -> null";
+        else
+            return String.valueOf(val) + " -> " + next.toString();
+    }
+
+    public static ListNode getLinkedList(int ... nodeList) {
+        if (nodeList.length == 0)
+            return null;
+
+        ListNode head = new ListNode(0);
+        ListNode node = head;
+        for (int nodeVal: nodeList) {
+            node.next = new ListNode(nodeVal);
+            node = node.next;
+        }
+        return head.next;
+    }
 }
+
