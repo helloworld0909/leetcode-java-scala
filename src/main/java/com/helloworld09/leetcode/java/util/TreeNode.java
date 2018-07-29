372c3b8e62ba9e1152340d69bbfc534e57b901e6
Zheng Luo <helloworld09@163.com>
2018-07-29 21:02:51 +0800

    Add TreeNode

---
+++ src/main/java/com/helloworld09/leetcode/java/util/TreeNode.java
@@ -0,0 +1,13 @@
+package com.helloworld09.leetcode.java.util;
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+
+}
