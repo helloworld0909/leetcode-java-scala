367d14dace27877849703ebdf8d704bc989a3c8f
Zheng Luo <helloworld09@163.com>
2018-08-02 15:18:16 +0800

    Add LC94 test

--- src/main/java/com/helloworld09/leetcode/java/LC94.java
+++ src/main/java/com/helloworld09/leetcode/java/LC94.java
@@ -5,6 +5,21 @@ import java.util.*;
 import com.helloworld09.leetcode.java.util.TreeNode;
 
 public class LC94 {
+    /**
+     * Given a binary tree, return the inorder traversal of its nodes' values.
+     * <p>
+     * Example:
+     * <p>
+     * Input: [1,null,2,3]
+     * 1
+     * \
+     * 2
+     * /
+     * 3
+     * <p>
+     * Output: [1,3,2]
+     * Follow up: Recursive solution is trivial, could you do it iteratively?
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> traversal = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
@@ -25,6 +40,11 @@ public class LC94 {
     }
 
     public static void main(String[] args) {
-
+        LC94 solution = new LC94();
+        TreeNode input = new TreeNode(0);
+        input.left = new TreeNode(1);
+        input.right = new TreeNode(2);
+        input.left.left = new TreeNode(3);
+        System.out.println(solution.inorderTraversal(input));
     }
 }
