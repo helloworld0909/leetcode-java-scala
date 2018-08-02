1301dc84a6e80ab414d9b4e60c07340c7867c415
Zheng Luo <helloworld09@163.com>
2018-08-02 15:15:54 +0800

    Add TreeNode

--- src/main/java/com/helloworld09/leetcode/java/util/TreeNode.java
+++ src/main/java/com/helloworld09/leetcode/java/util/TreeNode.java
@@ -1,5 +1,7 @@
 package com.helloworld09.leetcode.java.util;
 
+import java.util.*;
+
 
 public class TreeNode {
     public int val;
@@ -10,4 +12,47 @@ public class TreeNode {
         val = x;
     }
 
+    @Override
+    public String toString() {
+        return getPreorder().toString();
+    }
+
+    public List<Integer> getPreorder() {
+        List<Integer> preorder = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = this;
+
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                preorder.add(cur.val);
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (!stack.empty()) {
+                cur = stack.pop();
+                cur = cur.right;
+            }
+        }
+        return preorder;
+    }
+
+    public List<Integer> getInorder() {
+        List<Integer> inorder = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = this;
+
+        while (cur != null || !stack.empty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+                inorder.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return inorder;
+    }
 }
